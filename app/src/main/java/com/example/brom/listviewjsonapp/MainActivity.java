e43f485e1865887a95676412fe84e800da6fa2ca
b18veran <b18veran@student.his.se>
2019-04-29 15:54:31 +0200

    Refresh knapp

--- app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -66,6 +68,27 @@ public class MainActivity extends AppCompatActivity {
             });
 
         }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu){
+             getMenuInflater(). inflate(R.menu.menu_main, menu);
+             return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item){
+                int id = item.getItemId();
+            if(id == R.id.action_settings){
+                return true;
+            }
+            if(id == R.id.action_refresh){
+                new FetchData().execute();
+                return true;
+            }
+        return super.onOptionsItemSelected(item);
+        }
+
+
 
     private class FetchData extends AsyncTask<Void,Void,String>{
         @Override
